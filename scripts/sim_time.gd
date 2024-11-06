e6d1afa4108e15ec3a4d26e4140e1390e933de02
mulboor <wesjmulder@gmail.com>
2024-11-06 12:49:45 +0100

    Bla bla

--- scripts/sim_time.gd
+++ scripts/sim_time.gd
@@ -1,19 +1,22 @@
 extends Node
+class_name SimTime
 
 @export var frames_per_time_unit: int
 
 @onready var current_time: int
 @onready var current_frame: int
 
-# Called when the node enters the scene tree for the first time.
+signal TimeChanged(cur_time: int)
+
 func _ready() -> void:
 	current_frame = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Tel het aantal tijdseenheden
 func _process(delta: float) -> void:
 	if current_frame < frames_per_time_unit:
 		current_frame += 1
 	elif current_frame == frames_per_time_unit: 
 		current_time += 1
-		current_time = 0 
+		current_frame = 0 
+		
+		TimeChanged.emit(current_time)
