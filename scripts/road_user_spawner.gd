f577592c72a26103941437fca812c9f200b24a3c
mulboor <wesjmulder@gmail.com>
2024-11-17 14:55:59 +0100

    user_stats zijn nu deels random

--- scripts/road_user_spawner.gd
+++ scripts/road_user_spawner.gd
@@ -3,6 +3,7 @@ class_name RoadUserSpawner
 
 @export_category("Spawn variables")
 @export var time_between_spawns: float
+@export_range(0, 0.1) var random_deviation: float
 
 @export_category("User variables")
 @export var general_user_stats: GeneralRoadUserStats
@@ -37,11 +38,11 @@ func spawn_user() -> void:
 	
 	# Maak de weggebruiker
 	var inst_user: RoadUserVars = user.instantiate()
-	inst_user.max_speed = general_user_stats.max_speed
-	inst_user.accel = general_user_stats.accel
-	inst_user.deccel = general_user_stats.deccel
-	inst_user.visibility = general_user_stats.visibility
-	inst_user.reaction_time = general_user_stats.reaction_time
-	inst_user.stopping_distance = general_user_stats.stopping_distance
+	inst_user.max_speed = randfn(general_user_stats.max_speed, random_deviation)
+	inst_user.accel = randfn(general_user_stats.accel, random_deviation)
+	inst_user.deccel = randfn(general_user_stats.deccel, random_deviation)
+	inst_user.visibility = randfn(general_user_stats.visibility, random_deviation)
+	inst_user.reaction_time = randfn(general_user_stats.reaction_time, random_deviation)
+	inst_user.stopping_distance = randfn(general_user_stats.stopping_distance, random_deviation)
 	inst_user.path_follow = path_follow
 	add_child(inst_user)
