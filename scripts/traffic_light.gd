140d6f3d53c632bca5fd63e942e048a3de11cd34
mulboor <wesjmulder@gmail.com>
2025-01-07 16:55:22 +0100

    Stoplicht

--- scripts/traffic_light.gd
+++ scripts/traffic_light.gd
@@ -3,7 +3,7 @@ class_name TrafficLight
 
 @export var green_time: float
 @export var red_time: float
-@export var start_at_red: float
+@export var start_at_red: bool
 
 var cur_red_time: float
 var cur_green_time: float
