18af5ed664ebebb7ffc32c39e76530d29b2d323a
Jaylolz <jasondenhaan@icloud.com>
2025-01-23 13:21:35 +0100

    UI

--- ui/factors_test.gd
+++ ui/factors_test.gd
@@ -1,8 +1,11 @@
 extends Control
 
-func _on_snelheid_pressed() -> void:
-	get_tree().change_scene_to_file()
+var number = "Hello"
 
+func _ready():
+	print(number)
+	number = 100
+	print(number)
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/options_test.tscn")
