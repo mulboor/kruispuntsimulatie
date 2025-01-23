eceaba4af4aa8846b9e0a92f687ece6905b20700
Jaylolz <jasondenhaan@icloud.com>
2025-01-23 10:06:10 +0100

    UI
    Button

--- ui/options_test.gd
+++ ui/options_test.gd
@@ -1,9 +1,12 @@
 extends Control
 
+func _on_factors_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/factors_test.tscn")
+
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/test.tscn")
 
 
-func _on_factors_pressed() -> void:
-	get_tree().change
+
 	
