485eeb4f8411654d0fa9e351ee5b65d2e08e6531
Jaylolz <jasondenhaan@icloud.com>
2025-01-24 12:02:40 +0100

    UI

--- ui/factors_test.gd
+++ ui/factors_test.gd
@@ -6,8 +6,21 @@ extends Control
 func _ready():
 	line_edit.text_submitted.connect(_on_LineEdit_text_entered)
 	
+	line_edit.text_submitted.connect(_on_LineEdit2_text_entered)
+	
 func _on_LineEdit_text_entered(new_text: String) -> void:
 	label.text = "Your name is: " + new_text
+
+
+func _on_LineEdit2_text_entered(new_text: String) -> void:
+	label.text = "Your name is: " + new_text
+
+
+
+	
+
+
+
 
 
 func _on_back_pressed() -> void:
